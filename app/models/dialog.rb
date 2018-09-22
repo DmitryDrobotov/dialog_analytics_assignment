0b3698a29c79dfcbb6767e58483a6a945a716ac3
Polina <pollianna@tut.by>
2018-09-22 20:49:14 +0300

    Add alert-collors to the phrases interrupts and breaks

--- app/models/dialog.rb
+++ app/models/dialog.rb
@@ -2,12 +2,17 @@ class Dialog < ApplicationRecord
   has_many :phrases, -> { order(:start_in_sec) }, dependent: :destroy
   validates :name, presence: true
   
+  def duration(dialog)
+    time = dialog.phrases.last.end_in_sec
+    Time.at(time).utc.strftime("%H:%M:%S")
+  end
+
   def interruptions(dialog)
     interrupt = 0
     dialog.phrases.each do |phrase|
       id = phrase.id + 1
-      if Phrase.find_by(id: id)
-        interrupt += 1 if Phrase.find_by(id: id).start_in_sec <= phrase.end_in_sec
+      if dialog.phrases.find_by(id: id)
+        interrupt += 1 if Phrase.find(id).start_in_sec < phrase.end_in_sec
       end
     end
     max_interrupt = dialog.phrases.length - 1
@@ -15,17 +20,12 @@ class Dialog < ApplicationRecord
     return "#{interrupt}(#{percentage}%)"
   end
 
-  def duration(dialog)
-    time = dialog.phrases.last.end_in_sec
-    Time.at(time).utc.strftime("%H:%M:%S")
-  end
-
   def long_breaks(dialog)
     breaks = 0
     dialog.phrases.each do |phrase|
       id = phrase.id + 1
-      if Phrase.find_by(id: id)
-        breaks += 1 if (Phrase.find_by(id: id).start_in_sec - 5) < phrase.end_in_sec
+      if dialog.phrases.find_by(id: id)
+        breaks += 1 if Phrase.find_by(id: id).start_in_sec - 5 > phrase.end_in_sec
       end
     end
     max_breaks = dialog.phrases.length - 1
--- app/models/phrase.rb
+++ app/models/phrase.rb
@@ -5,4 +5,26 @@ class Phrase < ApplicationRecord
   validates :start_in_sec, presence: true, numericality: { only_integer: true }
   validates :end_in_sec, presence: true, numericality: { only_integer: true }
   validates :content, presence: true
+
+  def interrupt(phrase)
+    if phrase.befor_phrase(phrase)
+      phrase.befor_phrase(phrase).end_in_sec > phrase.start_in_sec
+    end
+  end
+
+  def breaks(phrase)
+    if phrase.befor_phrase(phrase)
+      phrase.befor_phrase(phrase).end_in_sec < phrase.start_in_sec - 5
+    end
+  end
+  
+  def following_phrase(phrase)
+    id = phrase.id + 1
+    Phrase.find(id)
+  end
+
+  def befor_phrase(phrase)
+    id = phrase.id - 1
+    Phrase.find(id)
+  end
 end
