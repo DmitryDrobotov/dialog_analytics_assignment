53588be58b5a4991feac21f71ddf571c79a3b8cf
Henadzi Simonovich <375295716621@yandex.by>
2018-09-26 23:50:54 +0300

    Add filter for Actor and statistic for interruptions and long breaks

--- app/models/phrase.rb
+++ app/models/phrase.rb
@@ -6,15 +6,9 @@ class Phrase < ApplicationRecord
   validates :end_in_sec, presence: true, numericality: { only_integer: true }
   validates :content, presence: true
 
+  # this return previous record or nil
   def previous 
     prev = Phrase.where('id < ?', id).last
-# binding.pry
     return prev if prev.dialog_id == dialog_id
-#   Phrase.joins(:dialog).find_by(phrases.id < ?self.id, phrases.dialog_id = ?dialog.dialog_id).last 
   end 
-
-#  def previous
-    # Phrase.where("id < ?", id).order("id DESC").first || Phrase.last
-    # Phrase.offset(self.id).limit(2).order(id: :desc).last
-#  end
 end
